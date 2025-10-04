d2a080b049fc1dd443d9e2f330e57af7e47d4b94
idelaniyariets <mr.kriper724@yandex.ru>
2025-10-04 22:53:23 +0300

    new templates

--- n19-n21_1.py
+++ n19-n21_1.py
@@ -38,7 +38,9 @@ print('20)', [s for s in range(31, 300) if (not(f(s, 1)) and f(s, 3))])
 print('21)', [s for s in range(31, 300) if not(f(s, 2)) and (f(s, 2) or f(s, 4))])
 '''
 Если в задании просят указать неверный ход или 
+
 неудачный, то строку с return меняем на return any(h) if m 
+
 % 2 != 0 else any(h). (Последнее all на any) 
 
 еще немаловажно сказать что ходы идут по порядку
