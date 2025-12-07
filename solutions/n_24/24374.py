06e7a8ff66d9f3b9b921c8bfc54163df1aef33eb
IdealniyAriets <mr.kriper724@gmail.com>
2025-12-07 21:08:23 +0300

    new solutions

---
+++ solutions/n_24/24374.py
@@ -0,0 +1,21 @@
+s = open("24_24374.txt").readline().strip()
+m = l = c = 0
+ans = []
+for r in range(2, len(s)-1):
+    if s[r].isdigit() and s[r-1].isalpha() and s[r-2].isalpha():
+        c += 1
+    if s[r-1].isdigit() and s[r].isalpha() and s[r+1].isdigit():
+        l = r+1
+        while s[l].isdigit():
+            l += 1
+        c = 0
+    while c>80:
+        l += 1
+        if s[l].isdigit():
+            while s[l].isdigit():
+                l += 1
+            c -= 1
+    if c == 80:
+        m = max(m, r-l+1)
+print(m)
+'''я слишком глуп для этого. не работает'''
